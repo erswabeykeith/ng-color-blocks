88d446628ecae079de0f486a663854a403d09e2a
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-29 09:22:00 -0600

    it works

--- server/public/scripts/controllers/settingsController.js
+++ server/public/scripts/controllers/settingsController.js
@@ -1,5 +1,14 @@
-colorBlocks.controller('SettingsController', ['$scope', function($scope) {
+colorBlocks.controller('SettingsController', ['$scope', "DataFactory", function($scope, DataFactory) {
   console.log("settings controller running");
 
-  
+var self = this;
+self.colors = DataFactory;
+self.addColor = function() {
+  self.colors.push(self.newColor);
+  self.newColor = "";
+  DataFactory = self.colors;
+  console.log(self.colors);
+
+
+}
 }]);
--- server/public/scripts/services/dataFactory.js
+++ server/public/scripts/services/dataFactory.js
@@ -1,6 +1,9 @@
-colorBlocks.factory('DataFactory', [function() {
+colorBlocks.factory('DataFactory', function() {
   console.log("data factory running");
-  var colors = [];
 
 
-}]);
+  var colors = ['red', 'blue', 'magenta', 'green', 'pink'];
+
+  return colors;
+
+});
